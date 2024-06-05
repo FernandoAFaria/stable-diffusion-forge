8258b34b948db374c0de09bb5eb2fd3bff9f1a06
Fernando Faria <fernandoafaria@yahoo.com>
2024-06-04 22:07:47 -0400

    update

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Starting Stable Diffusion WebUI"
-if [ ! -d "/app/sd-webui" ] || [ ! "$(ls -A "/app/sd-webui")" ]; then
+if [ ! -d "/app/sd-webui/modules" ] || [ ! "$(ls -A "/app/sd-webui/modules")" ]; then
   echo "Files not found, cloning..."
 
   # git clone https://github.com/lllyasviel/stable-diffusion-webui-forge.git /app/sd-webui
