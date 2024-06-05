4560e4927c19314676ffdd31130ad1cb01d0eb7c
Fernando Faria <fernandoafaria@yahoo.com>
2024-06-04 23:18:29 -0400

    fix

--- run.sh
+++ run.sh
@@ -2,15 +2,15 @@
 
 echo "Starting Stable Diffusion WebUI"
 
-if [false || ! -d "/app/sd-webui/" ] || [ ! "$(ls -A "/app/sd-webui")" ]; then
+if [false || ! -d "/sd-webui/" ] || [ ! "$(ls -A "/sd-webui")" ]; then
   echo "Files not found, cloning..."
 
   # Clone the repository
-  git clone https://github.com/lllyasviel/stable-diffusion-webui-forge.git /app/sd-webui
-  cd /app/sd-webui
+  git clone https://github.com/lllyasviel/stable-diffusion-webui-forge.git /sd-webui
+  cd /sd-webui
 
   # Ensure the script has execute permissions
-  chmod +x /app/sd-webui/webui.sh
+  chmod +x /sd-webui/webui.sh
 
   # Create virtual environment and install dependencies
   python3 -m venv venv
@@ -19,14 +19,14 @@ if [false || ! -d "/app/sd-webui/" ] || [ ! "$(ls -A "/app/sd-webui")" ]; then
   deactivate
 
   # Start the application
-  exec /app/sd-webui/webui.sh $ARGS
+  exec /sd-webui/webui.sh $ARGS
 else
   echo "Files found, starting..."
   pwd
   ls -la
-  cd /app/sd-webui
+  cd /sd-webui
   git pull
 
   # Start the application
-  exec /app/sd-webui/webui.sh $ARGS
+  exec /sd-webui/webui.sh $ARGS
 fi
